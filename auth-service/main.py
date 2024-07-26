201ec1b268c9feb5cc531d27588e644a68e46142
NogueiraMat <matheus.oek01@gmail.com>
2024-07-26 08:16:04 -0300

    feat(api): add api routes structure and cors policy

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import public, private
 
 
 app = FastAPI()
+
+app.add_middlware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_origins=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(public.router, prefix="/public", tags=["public"])
+app.include_router(private.router, prefix="/api", tags=["private"])
